25ac180630cf4aa706f0c79724f75776cc764f0f
codyasbin <himalthapa346@gmail.com>
2026-01-08 16:49:00 +0545

    tracking request headers

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -1,16 +1,21 @@
 import { cookies } from "next/headers";
+import { headers } from "next/headers";
 
 export async function GET() {
   const cookieStore = await cookies(); //getting cookies from the client request 
-  
+   const headersList = await headers(); //getting headers from the client request
   // Set a cookie for client browser
   cookieStore.set("testCookie", "hello", {
     httpOnly: true,
     secure: true,
     maxAge: 60 * 60 * 24 // 1 day
   });
+
+ 
+  const userAgent = headersList.get("user-agent") || "Unknown";
+
   
   // Read a cookie
   const testCookie = cookieStore.get("testCookie")?.value || "No cookie found";
-  return new Response(`Test cookie value: ${testCookie}`);
+  return new Response(`Test cookie value: ${testCookie}` + `\nUser-Agent: ${userAgent}`);
 }
