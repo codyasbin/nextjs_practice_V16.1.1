ee8a105ba2ecf48aa76da10d57f07567fb608fd8
codyasbin <himalthapa346@gmail.com>
2026-01-08 15:50:53 +0545

    layout added in app

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function Layout() {
+  return (
+    <html lang="en">
+      <body>
+        <h1>Welcome to Next.js 16.1.1 App Router!</h1>
+        <p>This is the root layout.</p>
+      </body>
+    </html>
+  );
+}
