6578d2a20a2fc3e055aa323af7321da3bb8b76a0
codyasbin <himalthapa346@gmail.com>
2026-01-09 23:05:08 +0545

    default page added for the parallel route

---
+++ app/@parrallelRoute/default.jsx
@@ -0,0 +1,3 @@
+export default function DefaultPage() {
+  return <h1>This is the default page for the @parallelRoute route!</h1>;
+}
---
+++ app/page/layout.jsx
@@ -0,0 +1,10 @@
+export default function Layout({chidren}) {
+    return (
+        <div>
+            <h2>
+                This is the layout for the /page route
+            </h2>
+            {chidren}
+        </div>
+    );
+}
