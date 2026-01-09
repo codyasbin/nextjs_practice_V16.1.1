a1cf9e4b9ec2b39cd9bee0dbee2a9774ea8535de
codyasbin <himalthapa346@gmail.com>
2026-01-09 23:41:51 +0545

    minor

--- app/page/layout.jsx
+++ app/page/layout.jsx
@@ -1,14 +1,9 @@
-export default function Layout({children, parrallelRoute}) { //parallel route folder has to be in the same level as this layout.jsx file
-    return (
-        <div className="flex border-4 border-green-500 gap-5">
-            <div>
-            {children}
-            </div>
-            <div>
-        {
-            parrallelRoute
-        }
-            </div>
-        </div>
-    );
+export default function Layout({ children, parrallelRoute }) {
+  //parallel route folder has to be in the same level as this layout.jsx file
+  return (
+    <div className="flex border-4 border-green-500 gap-5">
+      <div>{children}</div>
+      <div>{parrallelRoute}</div>
+    </div>
+  );
 }
