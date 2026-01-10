d59dfd3cacbb34f644aa276ca9c6cb1b0ebb8c61
codyasbin <himalthapa346@gmail.com>
2026-01-10 22:56:48 +0545

    comment added

--- app/propsHelper/page.jsx
+++ app/propsHelper/page.jsx
@@ -1,5 +1,6 @@
-export default async function Page(props) {
-    let data= await props.params
+export default async function Page(props) {  //props is an object that contains various properties including params and 
+// searchParams with promise support
+    let data= await props.params  
 console.log("params received in Page component:", data);
 return (
     <>
