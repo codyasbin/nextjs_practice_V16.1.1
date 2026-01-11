c6b4e3b6fe45b72df2c34d1348c252262106119e
codyasbin <himalthapa346@gmail.com>
2026-01-11 22:07:02 +0545

    oklch color added to the global css

--- app/css/page.jsx
+++ app/css/page.jsx
@@ -2,6 +2,9 @@ export default function Css() {
     return (
         <>
         <h1 className="text-(--danger-color) bg-primary">CSS Page</h1>
+        <div className="canvas">
+
+        </div>
         </>
     )
 }
