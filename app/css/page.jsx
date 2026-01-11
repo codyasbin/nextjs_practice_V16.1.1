b08a5e3004aaed428ce1111c520a2618817d00ad
codyasbin <himalthapa346@gmail.com>
2026-01-11 22:21:14 +0545

    comment added

--- app/css/page.jsx
+++ app/css/page.jsx
@@ -1,9 +1,9 @@
 export default function Css() {
     return (
         <>
-        <h1 className="text-(--danger-color) bg-primary">CSS Page</h1>
+        {/* --danger-color is a variable defined in app/globals.css */}
+        <h1 className="text-(--danger-color) bg-primary">CSS Page</h1>  
         <div className="canvas">
-
         </div>
         </>
     )
